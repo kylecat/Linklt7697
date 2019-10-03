943eacae8f945a83212d1c72656e2d0aa69956d6
KyleLiuCat <kyle.liu.coffeecat@gmail.com>
2019-10-03 19:26:27 +0800

    update ver 1.1
    fix bug at Sleep.init()
    add debug Option a initialize function

--- LSleep_test/LSleep.cpp
+++ LSleep_test/LSleep.cpp
@@ -1,3 +1,8 @@
+/***<< license >>*****************************************
+   Author：Kyle
+   Licenses：MIT
+   Version：1.1 (20191001)
+*********************************************************/
 /*
    reference:http://labs.mediatek.com/api/mt7697/group___s_l_e_e_p___m_a_n_a_g_e_r.html
 */
@@ -18,13 +23,21 @@ LSleepClass::LSleepClass()
    HAL_SLEEP_MANAGER_ERROR  An undefined error occurred.
    HAL_SLEEP_MANAGER_OK     The operation completed successfully.
 */
-void LSleepClass::init()
+void LSleepClass::init(bool _debug)
 {
+  _debugInfo = _debug;
+  
+  _headleIndex =  hal_sleep_manager_get_lock_status();
+  hal_sleep_manager_release_sleep_handle(_headleIndex);
+  
   if (hal_sleep_manager_init() == HAL_SLEEP_MANAGER_ERROR)
   {
     // Check the log for error handling.
-    Serial.println("Sleep Manager init failed");
-    return;
+    if (_debugInfo) Serial.println("[SleepManager] init failed");
+  }
+  else
+  {
+    if (_debugInfo) Serial.println("[SleepManager] init success");
   }
 }
 
@@ -36,7 +49,7 @@ int LSleepClass::setTime(uint32_t _ms)
   if (hal_sleep_manager_set_sleep_time(_ms) == HAL_SLEEP_MANAGER_OK)
   {
     _sleep_time = _ms;
-    Serial.println("Sleep Time was set at " + (String)_sleep_time + " ms");
+    if (_debugInfo) Serial.println("[SleepManager] Sleep Time was set at " + (String)_sleep_time + " ms");
     return 1;
   }
   else return 0;
@@ -50,40 +63,61 @@ int LSleepClass::setTime(uint32_t _ms)
    3 HAL_SLEEP_MODE_LEGACY_SLEEP   legacy sleep state
    4 HAL_SLEEP_MODE_NUMBER         for support range detection
 */
-int LSleepClass::setMode(int32_t _mode)
+int LSleepClass::setMode(uint8_t _mode)
 {
   _sleep_mode = _mode;
+  int _state;
   switch (_sleep_mode) {
     case 0:
-      Serial.println("Sleep Mode: NONE");
+      if (_debugInfo) Serial.println("[SleepManager] Sleep Mode: NONE");
+      _state = 1;
       break;
     case 1:
-      Serial.println("Sleep Mode: IDEL");
+      if (_debugInfo) Serial.println("[SleepManager] Sleep Mode: IDEL");
+      _state = 1;
       break;
     case 2:
-      Serial.println("Sleep Mode: SLEEP");
+      if (_debugInfo) Serial.println("[SleepManager] Sleep Mode: SLEEP");
+      _state = 1;
       break;
     case 3:
-      Serial.println("Sleep Mode: LEGACY_SLEEP");
+      if (_debugInfo) Serial.println("[SleepManager] Sleep Mode: LEGACY_SLEEP");
+      _state = 1;
       break;
     case 4:
-      Serial.println("Sleep Mode: NUMBER");
+      if (_debugInfo) Serial.println("[SleepManager] Sleep Mode: NUMBER");
+      _state = 1;
+      break;
+    default:
+      _state = 0;
       break;
   }
-
+  return _state;
 }
 
-void LSleepClass::sleep()
+int LSleepClass::sleep()
 {
-  if(hal_sleep_manager_is_sleep_locked())
-    {
-        Serial.println("Cannot enter the sleep mode, as the sleep is locked");
-    }else
-    {
-        if (hal_sleep_manager_set_sleep_time(_sleep_time) == HAL_SLEEP_MANAGER_OK)
-        {
-            hal_sleep_manager_enter_sleep_mode((hal_sleep_mode_t)_sleep_mode);
-        }
-    }
-}
+  int _state;
 
+  _headleIndex =  hal_sleep_manager_get_lock_status();
+  if (_debugInfo) Serial.println("[SleepManager] Handle Index:" + String(_headleIndex));
+
+  hal_sleep_manager_unlock_sleep(_headleIndex);                // 解除鎖定
+
+  if (hal_sleep_manager_is_sleep_locked())
+  {
+    if (_debugInfo) Serial.println("[SleepManager] Cannot enter the sleep mode, as the sleep is locked");
+
+    _state = 0;
+  } else if (hal_sleep_manager_set_sleep_time(_sleep_time) == HAL_SLEEP_MANAGER_OK)
+  {
+    if (_debugInfo) Serial.println("[SleepManager] Inter Sleep Mode");
+    hal_sleep_manager_enter_sleep_mode((hal_sleep_mode_t)_sleep_mode);
+    _state = 1;
+  }
+  else {
+    if (_debugInfo) Serial.println("[SleepManager] Cannot enter the sleep mode, set time error");
+    _state = 0;
+  }
+  return _state;
+}
--- LSleep_test/LSleep_test.ino
+++ LSleep_test/LSleep_test.ino
@@ -3,31 +3,25 @@
 LSleepClass Sleep;
 int count = 0;
 
-void setup() {
-  Serial.begin(9600);
-  Sleep.init();
-  Sleep.setTime(10000);
-  Sleep.setMode(3);
-  Serial.println(F("睡覺測試"));
+
+void enterSleep(int _ms, int _mode)
+{
+  float _sleepTime = _ms * 0.001;
+  Sleep.init(true);
+  Sleep.setTime(_ms);       // 設定時間
+  Sleep.setMode(_mode);     // 睡眠模式：0 不睡；1 待機狀態；2 wifi睡眠狀態；3 傳統睡眠狀態
+  Serial.print("晚安(");
+  Serial.print(_sleepTime, 3);
+  Serial.println(" s)");
   Sleep.sleep();
-  Serial.println(F("起床"));
+  Serial.println("起床");
 }
 
-void loop() {
-  unsigned long _t = millis();
+void setup() {
+  Serial.begin(9600);  
+  enterSleep(10,3);
+}
 
-  Serial.print(F("睡覺Loop:("));
-  Serial.print(count);
-  Serial.println(")");
-  Sleep.sleep();
-  Serial.println(F("起床"));
-
-  Serial.print(F("睡眠時間: "));
-  float Duration = (millis() - _t) / 1000;
-  Serial.print(Duration);
-  Serial.println(F("s"));
-
-  Serial.println(F("*****************"));
-  Serial.println();
-  count++;
+void loop() 
+{
 }
